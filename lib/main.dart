3bf3a4441a5ab0b07eb3909b016f687d714e6d65
Yash Sheth <yashsheth66@gmail.com>
2024-11-04 21:35:28 +0530

    add 2 separate screens

--- lib/main.dart
+++ lib/main.dart
@@ -25,35 +25,38 @@ class _MyAppState extends State<MyApp> {
           title: const Text("New Appbar"),
         ),
         body: Center(
-            child: Container(
-          height: double.infinity,
-          width: double.infinity,
-          color: Colors.red[400],
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: () => {
-                  setState(() {
-                    buttonName = 'Clicked';
-                  })
-                },
-                style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.red,
-                    backgroundColor: Colors.orange),
-                child: Text(buttonName),
-              ),
-              ElevatedButton(
-                  onPressed: () => {
-                        setState(() {
-                          buttonName = 'Clicked';
-                        })
-                      },
-                  child: Text(buttonName))
-            ],
-          ),
-        )),
+            child: currentIndex == 0
+                ? Container(
+                    height: double.infinity,
+                    width: double.infinity,
+                    color: Colors.red[400],
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () => {
+                            setState(() {
+                              buttonName = 'Clicked';
+                            })
+                          },
+                          style: ElevatedButton.styleFrom(
+                              foregroundColor: Colors.red,
+                              backgroundColor: Colors.orange),
+                          child: Text(buttonName),
+                        ),
+                        ElevatedButton(
+                            onPressed: () => {
+                                  setState(() {
+                                    buttonName = 'Clicked';
+                                  })
+                                },
+                            child: Text(buttonName))
+                      ],
+                    ),
+                  )
+                : Image.network(
+                    'https://firebasestorage.googleapis.com/v0/b/pixel-vault-db6a1.appspot.com/o/photos%2F08ff53a5-0902-4f2d-a2a0-153542576697_Yanfei.jpg?alt=media&token=9eaf2755-fadb-4057-b254-d602d7c04a1b')),
         bottomNavigationBar: BottomNavigationBar(
           items: const [
             BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),

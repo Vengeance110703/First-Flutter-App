166323be976cf0adef471ae911b68307c67d5b65
Yash Sheth <yashsheth66@gmail.com>
2024-11-04 20:51:53 +0530

    add body elevated button

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,9 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text("New Appbar"),
         ),
-        body: const Center(child: Text("Body")),
+        body: Center(
+            child:
+                ElevatedButton(onPressed: () => {}, child: const Text("Body"))),
         bottomNavigationBar: BottomNavigationBar(items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
           BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings')

4e9897794820d5c88efcd7f7aad457a471e22c14
Yash Sheth <yashsheth66@gmail.com>
2024-11-05 01:24:22 +0530

    add 2 pages

--- lib/main.dart
+++ lib/main.dart
@@ -14,64 +14,91 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: BasePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class BasePage extends StatefulWidget {
+  const BasePage({super.key});
+
+  @override
+  State<BasePage> createState() => _BasePageState();
+}
+
+class _BasePageState extends State<BasePage> {
   String buttonName = 'Click';
   int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("New Appbar"),
-        ),
-        body: Center(
-            child: currentIndex == 0
-                ? Container(
-                    height: double.infinity,
-                    width: double.infinity,
-                    color: Colors.red[400],
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        ElevatedButton(
-                          onPressed: () => {
-                            setState(() {
-                              buttonName = 'Clicked';
-                            })
-                          },
-                          style: ElevatedButton.styleFrom(
-                              foregroundColor: Colors.red,
-                              backgroundColor: Colors.orange),
-                          child: Text(buttonName),
-                        ),
-                        ElevatedButton(
-                            onPressed: () => {
-                                  setState(() {
-                                    buttonName = 'Clicked';
-                                  })
-                                },
-                            child: Text(buttonName))
-                      ],
-                    ),
-                  )
-                : Image.asset('images/Yanfei.jpg')),
-        // Image.network(
-        //     'https://firebasestorage.googleapis.com/v0/b/pixel-vault-db6a1.appspot.com/o/photos%2F08ff53a5-0902-4f2d-a2a0-153542576697_Yanfei.jpg?alt=media&token=9eaf2755-fadb-4057-b254-d602d7c04a1b')),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-            BottomNavigationBarItem(icon: Icon(Icons.image), label: 'Image')
-          ],
-          currentIndex: currentIndex,
-          onTap: (index) => {
-            setState(() {
-              currentIndex = index;
-            })
-          },
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("New Appbar"),
       ),
-      debugShowCheckedModeBanner: false,
+      body: Center(
+          child: currentIndex == 0
+              ? Container(
+                  height: double.infinity,
+                  width: double.infinity,
+                  color: Colors.red[400],
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () => {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  const SecondPage()))
+                        },
+                        style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.red,
+                            backgroundColor: Colors.orange),
+                        child: const Text('Navigate'),
+                      ),
+                      ElevatedButton(
+                          onPressed: () => {
+                                setState(() {
+                                  buttonName = 'Clicked';
+                                })
+                              },
+                          child: Text(buttonName))
+                    ],
+                  ),
+                )
+              : Image.asset('images/Yanfei.jpg')),
+      // Image.network(
+      //     'https://firebasestorage.googleapis.com/v0/b/pixel-vault-db6a1.appspot.com/o/photos%2F08ff53a5-0902-4f2d-a2a0-153542576697_Yanfei.jpg?alt=media&token=9eaf2755-fadb-4057-b254-d602d7c04a1b')),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.image), label: 'Image')
+        ],
+        currentIndex: currentIndex,
+        onTap: (index) => {
+          setState(() {
+            currentIndex = index;
+          })
+        },
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("2nd Page"),
+      ),
     );
   }
 }
